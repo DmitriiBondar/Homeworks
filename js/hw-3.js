b4974b946b55630b97c68ac22ed711757231b839
Sakvoyazh <ghty98@mail.ru>
2024-02-16 21:59:29 +0300

    made hw-3-js

---
+++ js/hw-3.js
@@ -0,0 +1,91 @@
+// _____Problem_1_____
+
+// let password = 'Пароль';
+// let userPass = prompt('Введите пароль');
+// let isCorrect = (userPass === password) ? 'Пароль верный' : 'Пароль неверный';
+// alert(isCorrect);
+
+
+// _____Problem_2_____
+
+// let c = prompt('Введите число');
+// let isOK = (c > 0 && c < 10) ? 'Верно' : 'Неверно';
+// console.log(isOK);
+
+
+// _____Problem_3_____
+
+// let d = prompt('Введите число');
+// let e = prompt('Введите число');
+// let isMore = (d > 100 || e > 100) ? 'Верно' : 'Неверно';
+// console.log(isMore);
+
+// _____Problem_4_____
+
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+
+
+// _____Problem_5_____
+
+// let monthNumber = Number(prompt('Введите число месяца'));
+// switch (monthNumber) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         console.log('Winter');
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         console.log('Spring');
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         console.log('Summer');
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         console.log('Autumn');
+//         break;
+//     default:
+//         console.log('Ты шо такое пишешь?');
+//         break;
+// }
+
+// _____***Problem_7***_____
+
+// let num = Number(prompt('Введите любое число'));
+// if(isNaN(num)) {
+//     alert('Введите корректное число!')
+// } else {
+//     if (num % 2 == 0) {
+//         alert('Число чётное');
+//     } else {
+//         alert('Число нечётное');
+//     }
+// }
+
+// _____***Problem_8***_____
+
+// let isAndroid = confirm('Вы пользуетесь Android?');
+// let Android = (isAndroid === true) ? 'Установите версию приложения для Android по ссылке' : 'Установите версию приложения для iOS по ссылке';
+// alert(Android);
+
+
+// _____***Problem_9***_____
+
+// let clientDeviceYear = prompt('Введите год выпуска Вашего телефона');
+// let isAndroid = confirm('Вы пользуетесь Android?');
+// if(isAndroid === true && clientDeviceYear >= 2015) {
+//     alert('Установите версию приложения для Android по ссылке');
+// } else if (isAndroid === false && clientDeviceYear >= 2015) {
+//     alert('Установите версию приложения для iOS по ссылке');
+// } else if (isAndroid === true && clientDeviceYear < 2015) {
+//     alert('Установите облегченную версию приложения для Android по ссылке');
+// } else {
+//     alert('Установите облегченную версию приложения для iOS по ссылке');
+// }
