8c593324d9f3ef50eadfcc00a99fa26756ad7f9f
Sakvoyazh <ghty98@mail.ru>
2024-02-25 16:43:33 +0300

    some new code

--- js/index-game.js
+++ js/index-game.js
@@ -1,6 +1,15 @@
-function button(el) {
-    el.innerHTML = "Let's go!";
-    el.style.background = '#008282';
-    el.style.color = 'pink';
+// document.getElementById('words').addEventListener("click", button);
+// function button() {
+//     let el = document.getElementById('words');
+//     el.innerHTML = "Let's go!";
+//     el.style.background = '#008282';
+//     el.style.color = 'pink';
 
+// }
+
+var buttons = document.getElementsByClassName('allButtons');
+
+for (let i = 0; i < buttons.length; i++) {
+    const element = buttons[i];
+    
 }
