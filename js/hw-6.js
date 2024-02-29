484b3ef6e7ae932d7fa6f8836c36c134cf953425
Sakvoyazh <ghty98@mail.ru>
2024-02-29 17:21:11 +0300

    problem-4

--- js/hw-6.js
+++ js/hw-6.js
@@ -23,13 +23,24 @@
 
 // _______Problem_3_______
 
-const arr3 = [1, 3, 5, 10, 20];
+// const arr3 = [1, 3, 5, 10, 20];
 
-const joinArr3 = arr3.join(' ');
-console.log(joinArr3);
+// const joinArr3 = arr3.join(' ');
+// console.log(joinArr3);
 
 
 // _______Problem_4_______
+
+const arr4 = [];
+
+for(let i = 0; i < 3; i++) {
+    arr4[i] = [];
+    for (let j = 0; j < 3; j++) {
+        arr4[i][j] = 1;
+    }
+}
+console.log(arr4);
+
 
 // _______Problem_5_______
 
